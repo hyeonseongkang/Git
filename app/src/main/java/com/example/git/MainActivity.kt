49515d65941d479e8b8d8ab38327c7f81a2f6346
HyeonSeongKang <gt9697@naver.com>
2023-08-01 22:17:11 +0900

    test2

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var test: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test = "test2"
     }
 }
